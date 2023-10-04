062d0bf2bd20054dd43d7060f592bf1eda7da1a1
Idressnoori <146789328+Idressnoori@users.noreply.github.com>
2023-10-04 22:09:16 +0400

    Update MyFistClass.java

--- src/tek/java/MyFistClass.java
+++ src/tek/java/MyFistClass.java
@@ -3,5 +3,6 @@ package tek.java;
 public class MyFistClass {
     public static void main(String [] args){
         System.out.println("The new changes has been made");
+        System.out.println("added by Idress Noori");
     }
 }
